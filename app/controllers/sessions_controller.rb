39ca86ca1df88a5ba7266b87f6ef44d015f14003
Nathan Glynn <nathan.glynn@kellogg.northwestern.edu>
2025-03-09 20:10:58 +0000

    log in log out and sign up working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
+    #find the user by their email
+    @user = User.find_by({ "email" => params["email"] })
 
+    #if the email exists and the password matches, we can log in this user for the session
+    if @user && BCrypt::Password.new(@user["password_digest"]) == params["password"]
+      #unique session id for th user
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome back, #{@user.first_name}!"
+      redirect_to "/places" 
+    else
+      flash[:notice] = "Invalid email or password."
+      redirect_to "/login"
+    end
+  end
+  
+  #logging the user out should terminate the session
   def destroy
+  session[:user_id] = nil  
+  flash[:notice] = "Succesfully logged out."
+  redirect_to "/login"
   end
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
-    @user["password"] = params["password"]
-    @user.save
-    redirect_to "/"
+    #verifies if email address is already taken
+    if User.find_by({ "email" => params["email"]}).nil?
+      #create new user if email is available
+      @user = User.new
+      @user["first_name"] = params["first_name"]
+      @user["last_name"] = params["last_name"]
+      @user["email"] = params["email"]
+      @user["password_digest"] = BCrypt::Password.create(params["password"]) 
+
+      #save and log in the new user
+      if @user.save
+        session[:user_id] = @user.id 
+        flash[:notice] = "Welcome, #{@user.first_name}!"
+        redirect_to "/places"
+      else
+        render :new
+      end
+
+    else
+      #this would be the case that the email address is already taken
+      flash[:notice] = "Email address is taken"
+      redirect_to "/users/new"
+    end
   end
 end
