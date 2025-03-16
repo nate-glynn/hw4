63f95c7c58e7c8caeafc788f863ced0ba3649a14
Nathan Glynn <nathan.glynn@kellogg.northwestern.edu>
2025-03-16 18:47:33 +0000

    Updated entries and authentication with known bug in places

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
-
+  helper_method :current_user
+ 
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
+
+  #def current_user
+   # puts "------------------ code before every request ------------------"
+  #end
 end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,24 +10,31 @@ class EntriesController < ApplicationController
 
   def new
     @entry = Entry.new
+    @place = Place.find(params[:place_id])
   end
 
   def create
-    @entry = current_user.entries.build(entr)
+    #@entry = current_user.entries.build(entry)
+    #@entry = Entry.new
+    #@entry["title"] = params["title"]
+    #@entry["description"] = params["description"]
+    #@entry["occurred_on"] = params["occurred_on"]
+    #@entry["place_id"] = params["place_id"]
+    #@entry["user_id"] = current_user.id
+
+    @entry = current_user.entries.build(
+      title: params["title"],
+      description: params["description"],
+      occurred_on: params["occurred_on"],
+      place_id: params["place_id"]
+    )
     
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = current_user.id
-   
     if @entry.save
       redirect_to "/places/#{@entry.place_id}"
     else
       render :new
     end
-    
+
   end
 
 end
