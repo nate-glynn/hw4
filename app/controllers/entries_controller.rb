a51cd8b797c76eb19a96d31bf85a8512a862fbdb
Nathan Glynn <nathan.glynn@kellogg.northwestern.edu>
2025-03-16 18:10:37 +0000

    Updated Entries Contorller with Authentication

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,33 @@
 class EntriesController < ApplicationController
 
+  def index
+    @entries = current_user.entries
+  end
+
+  def show
+    @entries = current_user.entries.find(params[:id])
+  end
+
   def new
+    @entry = Entry.new
   end
 
   def create
+    @entry = current_user.entries.build(entr)
+    
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @entry["user_id"] = current_user.id
+   
+    if @entry.save
+      redirect_to "/places/#{@entry.place_id}"
+    else
+      render :new
+    end
+    
   end
 
 end
